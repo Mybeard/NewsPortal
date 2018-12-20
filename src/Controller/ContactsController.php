0a2f73ddefb55a7aeea4e7dfb046deff12cea04b
Oleksii Maiboroda <alexeymayboroda23@gmail.com>
2018-12-20 17:20:49 +0200

    Contacts page added

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -1,24 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: boroda
- * Date: 18.12.18
- * Time: 21:05
- */
-
 namespace App\Controller;
-
-
-use App\Dto\Contacts;
+use App\Form\ContactForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ContactsController extends AbstractController
 {
-        public function contacts(Contacts) : Response
-        {
-            return $this->render('default/contacts.html.twig');
+    public function index(Request $request): Response
+    {
+        $formSuccess = false;
+        $form = $this->createForm(ContactForm::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formSuccess = true;
         }
-
-
+        return $this->
+        render(
+            'contact/contacts.html.twig',
+            [
+                'form' => $form->createView(),
+                'formSuccess' => $formSuccess,
+            ]
+        );
+    }
 }
